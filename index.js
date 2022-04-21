07a2e45dd3c5e1e8488f6a5e9fedef62631a74d8
Alina <pisarenko.alina@gmail.com>
2022-04-21 15:10:55 -0400

    commit tree 1f03e42479e728a57914d606aa5c8390547ab2be

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let originalArray = word.split(``);
+  let newArray = originalArray.reverse();
+  let newWord = newArray.join(``);
+  if (newWord === word) return true;
+  else return false;
 }
 
 /* 
-  Add your pseudocode here
+1. create funcion that takes one argument, a string
+2. split the word in letters and put it in array(create new array)
+3. put letters together from the last item of array to the first
+4. compare to the new string to the one I passed
+5. return true if its the same, otherwise return false
 */
 
 /*
-  Add written explanation of your solution here
+ If the new word that I created is equal to an argument, then function returns true
 */
 
 // You can run `node index.js` to view these console logs
